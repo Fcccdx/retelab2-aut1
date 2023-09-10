33cb1b16ad713391b246ec7090e64f2164929006
Fcccdx <nemeth.a.fabian@gmail.com>
2023-09-10 19:13:47 +0200

    Task no.1
    finished

---
+++ src/main/java/hu/bme/aut/retelab2/domain/Ad.java
@@ -0,0 +1,73 @@
+package hu.bme.aut.retelab2.domain;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+public class Ad
+{
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String title;
+    private String description;
+    private int price;
+
+    @CreationTimestamp
+    private Date creationDate;
+
+    public Long getId()
+    {
+        return id;
+    }
+
+    public void setId(Long id)
+    {
+        this.id = id;
+    }
+
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public void setTitle(String title)
+    {
+        this.title = title;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }
+
+    public int getPrice()
+    {
+        return price;
+    }
+
+    public void setPrice(int price)
+    {
+        this.price = price;
+    }
+
+    public Date getCreationDate()
+    {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate)
+    {
+        this.creationDate = creationDate;
+    }
+}
